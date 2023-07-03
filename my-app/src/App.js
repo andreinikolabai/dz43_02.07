a2a1a7e3fb50c7b527bd055b24994ae78836d3c9
andreinikolabai <andreiplus2000@gmail.com>
2023-07-03 21:47:29 +0300

    update

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Contacts from './Contacts';
+
+
+const App = () => {
+  return (
+      <div className="App">
+        <Contacts />
+      </div>
+  );
+};
+
+export default App;
